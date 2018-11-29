f673049f46898749b930229d439b58acc311cda0
Zach Frazier <frazier.zach@gmail.com>
2018-11-28 22:23:13 -0700

    created singleton

--- TaskBoard/Hubs/BoardHub.cs
+++ TaskBoard/Hubs/BoardHub.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json.Linq;
+using TaskBoard.Models;
 
 namespace TaskBoard.Hubs
 {
     public class BoardHub : Hub
     {
-        public void Hello()
+        Singleton instance = Singleton.Instance;
+
+        public void Send(string json)
         {
-            Clients.All.hello();
+            JObject jobject = JObject.Parse(json);
+            Clients.All.broadcastC(instance.Values);
         }
     }
 }
---
+++ TaskBoard/Models/Singletoncs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskBoard.Models
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public List<Board> Boards { get; set; }        
+        public List<Group> Groups { get; set; }
+
+        private Singleton()
+        {
+            this.Groups = new List<Group>();
+            this.Boards = new List<Board>();
+        }
+    }
+}
